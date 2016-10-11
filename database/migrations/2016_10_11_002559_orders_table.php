8a72b52a7a7f399b880ec7db6093001e5b3ca5c4
learnsomethings <joshuamcdonald69124@gmail.com>
2016-10-11 00:47:16 +0000

    User Login Page Code

---
+++ database/migrations/2016_10_11_002559_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class OrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('order_id');
+            $table->integer('address_id');
+            $table->integer('user_id')->references('id')->on('users');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
